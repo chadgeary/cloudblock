3b58acf451f4c7654c4d18ff5aa70b27e88b35a2
chadgeary <chad.geary@gmail.com>
2020-12-08 00:36:15 +0000

    gcp default storage iam and aws amd64 backups

--- gcp/gcp-storage.tf
+++ gcp/gcp-storage.tf
@@ -19,6 +19,9 @@ resource "google_storage_bucket" "ph-bucket" {
 resource "google_storage_bucket_acl" "ph-bucket-acl" {
   bucket                            = google_storage_bucket.ph-bucket.name
   role_entity                       = [
+    "OWNER:project-owners-${google_project.ph-project.number}",
+    "OWNER:project-editors-${google_project.ph-project.number}",
+    "READER:project-viewers-${google_project.ph-project.number}",
     "OWNER:user-${var.gcp_user}",
     "OWNER:user-${google_service_account.ph-service-account.email}"
   ]
