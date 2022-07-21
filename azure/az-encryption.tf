1a1ad1ef227fa6bf31c4eb853f13b06acbcd8d03
Dvir S <dviros@me.com>
2022-07-21 14:28:45 +0300

    Update azure/az-encryption.tf
    Co-authored-by: Chad <chad.geary@gmail.com>

--- azure/az-encryption.tf
+++ azure/az-encryption.tf
@@ -46,7 +46,6 @@ resource "azurerm_key_vault_access_policy" "ph-vault-disk-des" {
     "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"
   ]
   secret_permissions = [
-	"Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
   ]
   certificate_permissions = [
   ]
