585d09b2c699f2ffe5b0f384f1fb26eee37b3213
eemersonrosa <101534880+eemersonrosa@users.noreply.github.com>
2024-07-06 11:46:08 -0600

    fix for issue in ACL S3 bucket (#111)

--- aws/aws-s3.tf
+++ aws/aws-s3.tf
@@ -4,10 +4,19 @@ resource "aws_s3_bucket" "ph-bucket" {
   force_destroy = true
 }
 
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.ph-bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
 # acl
 resource "aws_s3_bucket_acl" "ph-bucket" {
   bucket = aws_s3_bucket.ph-bucket.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 # versioning
